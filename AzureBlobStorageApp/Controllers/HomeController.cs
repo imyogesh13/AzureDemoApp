1685cbf91415a3fecc55e288189e1e79a552dec9
Yogesh Patil <imyogesh13@outlook.com>
2020-04-08 17:10:04 +0530

    Changes

--- AzureBlobStorageApp/Controllers/HomeController.cs
+++ AzureBlobStorageApp/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace AzureBlobStorageApp.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
-        { 
+        {  
             return View();
         }
 
